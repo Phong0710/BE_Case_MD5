a1555177a3bfce91ef70142c0cf83e9b67ee1b6d
phong0710 <phonglh071099@gmail.com>
2023-05-25 16:42:17 +0700

    chua lam gi

--- index.ts
+++ index.ts
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
+
 app.use('', router)
 app.listen(8000, () => {
     console.log('Server is running')
