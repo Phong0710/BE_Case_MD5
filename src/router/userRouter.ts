8d1fd78b09ab5b05a16646f4232dfdc73bb0893b
duylo <duyvu06497@gmail.com>
2023-05-25 10:56:05 +0700

    xong khung code

--- src/router/userRouter.ts
+++
@@ -1,6 +0,0 @@
-import {Router} from "express";
-import userController from "../controller/userController";
-
-export const userRouter = Router();
-userRouter.post('/register', userController.register);
-userRouter.post('/login', userController.login);
