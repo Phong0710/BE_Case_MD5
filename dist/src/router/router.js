ec93c46059be40953496cf78bcd3901daca0ae50
phong0710 <phonglh071099@gmail.com>
2023-05-26 14:46:13 +0700

    sau khi merge hung

--- dist/src/router/router.js
+++ dist/src/router/router.js
@@ -8,10 +8,8 @@ const userRouter_1 = __importDefault(require("./userRouter"));
 const houseRouter_1 = __importDefault(require("./houseRouter/houseRouter"));
 const ContractRouter_1 = __importDefault(require("./ContractRouter"));
 const router = (0, express_1.Router)();
-router.use('/users', userRouter_1.default);
-router.use('/house', houseRouter_1.default);
-router.use('/contract', ContractRouter_1.default);
 router.use('', userRouter_1.default);
 router.use('/house', houseRouter_1.default);
+router.use('/contract', ContractRouter_1.default);
 exports.default = router;
 //# sourceMappingURL=router.js.map
